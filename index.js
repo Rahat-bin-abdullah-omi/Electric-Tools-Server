752f7698b387d546827751248556e2c4197921cf
Rahat Bin Abdullah Omi <rahatomi123@gmail.com>
2022-05-26 21:26:42 +0600

    single order api done

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ require('dotenv').config();
 var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+
+app.use(cors())
+app.use(express.json())
+
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    console.log(authHeader)
+    if (!authHeader) {
+        res.status(401).send({ message: "unAuthorization" })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbiddenAccess' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
