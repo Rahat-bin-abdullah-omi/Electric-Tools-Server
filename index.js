9111710bf12bead80828b9ae28e159860653a19e
Rahat Bin Abdullah Omi <rahatomi123@gmail.com>
2022-05-26 21:30:02 +0600

    add review api

--- index.js
+++ index.js
@@ -39,3 +39,57 @@ async function run() {
         const reviewCollection = client.db("Products").collection("reviews");
         const profileCollection = client.db("Products").collection("addProfile");
         const userCollection = client.db("Products").collection("AllUsers");
+        //   all get method   / /
+        app.get('/tools', async(req, res) => {
+            const tools = await toolsCollection.find().toArray();
+            res.send(tools)
+        })
+        app.get('/tools/:id', async(req, res) => {
+                const id = req.params.id;
+                const query = { _id: ObjectId(id) };
+                const findTool = await toolsCollection.findOne(query);
+                res.send(findTool)
+            })
+            // show purchase orders
+        app.get('/orders', async(req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const orders = await PurchaseCollection.find(query).toArray();
+            res.send(orders)
+        })
+        app.get('/orders/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const orders = await PurchaseCollection.find(query).toArray();
+            res.send(orders)
+        })
+
+        app.get('/reviews', async(req, res) => {
+            const reviews = await reviewCollection.find().toArray();
+            res.send(reviews)
+        })
+
+        app.get('/profile', async(req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const profile = await profileCollection.find(query).toArray();
+            res.send(profile)
+        })
+
+        app.get('/findAdmin/:email', async(req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user ? .role === 'admin';
+            res.send({ admin: isAdmin })
+
+        })
+
+        app.get('/showAllUser', async(req, res) => {
+            const allUser = await userCollection.find({}).toArray();
+            res.send(allUser)
+        })
+
+        app.get('/allOrders', async(req, res) => {
+            const allOrders = await PurchaseCollection.find().toArray();
+            res.send(allOrders)
+        })
